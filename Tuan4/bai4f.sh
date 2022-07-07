b99e6dedaf10b68485b0e7c1c670bb46e9d5a5a8
Tuan Badboy <91329722+TuanGoodboy@users.noreply.github.com>
2022-07-07 09:48:07 +0700

    Tuan 4

--- Tuan4/bai4f.sh
+++ Tuan4/bai4f.sh
@@ -1,18 +1,2 @@
 echo "Nhap vao ten mot folder: "
 read DIR
-if [ -d "$DIR" ]; then
-
-  echo "Installing config files in ${DIR}..."
-	for entry in `ls $DIR`; do
-	
-	    if [ ! -s $DIR/$entry ]
-		then
-			echo $entry
-		fi
-	done
-else
-
-  echo "Error: ${DIR} not found. Can not continue."
-  exit 1
-fi
-
