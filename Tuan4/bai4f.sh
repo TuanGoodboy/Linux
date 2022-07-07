1ef777833b9c9572afbb541cf823d7d37c4eca60
Tuan Badboy <91329722+TuanGoodboy@users.noreply.github.com>
2022-07-07 08:22:46 +0700

    Update bai4f.sh

--- Tuan4/bai4f.sh
+++ Tuan4/bai4f.sh
@@ -1,7 +1,7 @@
 echo "Nhap vao ten mot folder: "
 read DIR
 if [ -d "$DIR" ]; then
-  ### Take action if $DIR exists ###
+
   echo "Installing config files in ${DIR}..."
 	for entry in `ls $DIR`; do
 	
@@ -11,7 +11,7 @@ if [ -d "$DIR" ]; then
 		fi
 	done
 else
-  ###  Control will jump here if $DIR does NOT exists ###
+
   echo "Error: ${DIR} not found. Can not continue."
   exit 1
 fi
