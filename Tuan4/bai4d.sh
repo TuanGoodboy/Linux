65aacf2f726fa440e409ae58ff1b427abad6e289
Tuan Badboy <91329722+TuanGoodboy@users.noreply.github.com>
2022-07-07 09:40:29 +0700

    Tuan 4

--- Tuan4/bai4d.sh
+++ Tuan4/bai4d.sh
@@ -1,5 +1,32 @@
 #!bin/bash
 date
 set `date`
-echo "Hom nay la thu $1 ngay $3 thang $2 nam $6"
+case $1 in
+	Mon)
+		a="thu hai"
+		;;
+	Tue)
+		a="thu ba"
+		;;
+	Wed)
+		a="thu tu"
+		;;
+	Thu)
+		a="thu nam"
+		;;
+	Fri)	
+		a="thu sau"
+		;;
+	Sat)	
+		a="thu bay"
+		;;
+	Sun)	
+		a="chu nhat"
+		;;
+	*)
+		a=""
+		;;
+
+esac
+echo "Hom nay la thu $a ngay $3 thang $2 nam $6"
 echo "Gio hien nay $4"
